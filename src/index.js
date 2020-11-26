5baa4bc9314b1dd502f682ad593943e38d1ed4c3
islamashraful <rx.ju.edu@gmail.com>
2020-11-26 22:47:57 +0700

    Action types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+import store from "./store";
+import * as actions from "./actionTypes";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
+});
+
+store.dispatch({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: "Bug 1",
+  },
+});
+
+store.dispatch({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
