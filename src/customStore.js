3196f37ebf9eca3f543846946ab67f72c0ed8023
islamashraful <rx.ju.edu@gmail.com>
2020-11-26 23:54:15 +0700

    Basic redux implemention

---
+++ src/customStore.js
@@ -0,0 +1,28 @@
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+
+    for (let i = 0; i < listeners.length; i++) {
+      listeners[i]();
+    }
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+}
+
+export default createStore;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
+// import store from "./store";
+// import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed", store.getState());
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed", store.getState());
+// });
+
+// store.dispatch(bugAdded("Bug 1"));
+
+// store.dispatch(bugResolved(1));
+
+// console.log(store.getState());
+
+import reducer from "./reducer";
+import createStore from "./customStore";
+import * as actions from "./actions";
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log("Store changed!");
 });
 
-store.dispatch(bugAdded("Bug 1"));
-
-store.dispatch(bugResolved(1));
+store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
