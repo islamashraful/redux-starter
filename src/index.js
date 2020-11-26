4f71998bf9f347a26733fb0c5776b2119493d6d5
islamashraful <rx.ju.edu@gmail.com>
2020-11-26 22:53:51 +0700

    Action creators

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug 1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
