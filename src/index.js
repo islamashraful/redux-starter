b00efa0caf80433bd1f028bf2ae6a62fa0230e73
islamashraful <rx.ju.edu@gmail.com>
2020-11-27 02:42:50 +0700

    Config dev tool

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-// import store from "./store";
-// import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed", store.getState());
-// });
-
-// store.dispatch(bugAdded("Bug 1"));
-
-// store.dispatch(bugResolved(1));
-
-// console.log(store.getState());
-
-import reducer from "./reducer";
-import createStore from "./customStore";
-import * as actions from "./actions";
-
-const store = createStore(reducer);
-
-store.subscribe(() => {
-  console.log("Store changed!");
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed", store.getState());
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 1"));
+
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
